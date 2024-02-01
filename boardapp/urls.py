603fadb8def87e9db383a918a3f01228e4429bb2
usamaru123 <usam260@gmail.com>
2024-02-01 14:54:39 +0900

    a

---
+++ boardapp/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views import signupfunc,loginfunc,listfunc,logoutfunc,detailfunc,goodfunc,BoardCreate
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('signup/', signupfunc,name='signup'),
+    path('login/', loginfunc,name='login'),
+    path('list/', listfunc,name='list'),
+    path('logout/',logoutfunc, name='logout'),
+    path('detail/<int:pk>',detailfunc,name='detail'),
+    path('good/<int:pk>',goodfunc,name='good'),
+    path('create/',BoardCreate.as_view(),name='create')
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
